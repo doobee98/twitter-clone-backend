ed04054f0faef0086b2ecf314bc922c0d118c308
doobee98 <doobee98@naver.com>
2021-05-29 23:28:07 +0900

    CHORE: setting - firebase, env

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKEY,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
+export const databaseService = firebase.database();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import './env';
 import express from 'express';
 import cors from 'cors';
+import { authService } from './firebase';
 
 const app = express();
 const port = 8000;
@@ -9,6 +11,10 @@ app.use(cors());
 app.get('/', (req, res) => {
   console.log('/');
   res.send('Hello World');
+});
+
+app.get('/auth', (req, res) => {
+  res.send(authService.currentUser);
 });
 
 app.listen(port, () => {
