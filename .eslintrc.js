f2395fb946308509e57105cc8b5f93d04c76b287
2doo <doobee98@naver.com>
2021-05-29 12:34:58 +0900

    CHORE: eslint, prettier setting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'linebreak-style': 0,
+    'import/prefer-default-export': 0,
+    'prettier/prettier': 0,
+    'import/extensions': 0,
+    'no-use-before-define': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-shadow': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+  },
+};
