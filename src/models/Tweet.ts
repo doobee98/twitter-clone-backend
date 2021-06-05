da4e1d35408b0c80f1f2dedabcd1dd79b2fc084c
doobee98 <doobee98@naver.com>
2021-06-05 12:57:52 +0900

    FEAT(tweet): Tweet Model

---
+++ src/models/Tweet.ts
@@ -0,0 +1,28 @@
+import User from './User';
+
+/**
+ * @typedef Tweet
+ * @property {string} type.required - tweet | retweet | reply
+ * @property {string} tweet_id.required
+ * @property {string} tweeted_at.required
+ * @property {User.model} writer.required
+ * @property {string} content.required
+ * @property {Array.<string>} image_src_list
+ * @property {number} reply_count.required
+ * @property {number} retweet_count.required
+ * @property {number} like_count.required
+ */
+
+export default interface Tweet {
+  type: 'tweet' | 'retweet' | 'reply';
+  tweet_id: string;
+  tweeted_at: string;
+  writer: User;
+
+  content: string;
+  image_src_list?: string[];
+
+  reply_count: number;
+  retweet_count: number;
+  like_count: number;
+}
